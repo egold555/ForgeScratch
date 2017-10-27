af27d4b6e8489121db107b467e4f07435f36efbe
Eric Golde <eric@golde.org>
2017-10-26 19:22:13 -0700

    Create tutorial package

---
+++ ForgeScratch/forge/src/main/java/org/golde/forge/scratchforge/tutorialmod/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The tutorial mod for scratchforge.
+ * This makes new gui screens to improve user experience and teach them how to use the program
+ * Not neeeded for mod releases
+ */
+/**
+ * @author Eric
+ *
+ */
+package org.golde.forge.scratchforge.tutorialmod;
