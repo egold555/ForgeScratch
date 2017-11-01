fd9aa99f863039f460bd93f9d0e2b0ae7367486f
Eric Golde <eric@golde.org>
2017-11-01 11:47:02 -0700

    Working on entity related options, like block options

---
+++ ForgeScratch/forge/src/main/java/org/golde/forge/scratchforge/mods/aitest/CommonProxy.java
@@ -0,0 +1,290 @@
+package org.golde.forge.scratchforge.mods.aitest;
+
+import org.golde.forge.scratchforge.base.common.block.*;
+import org.golde.forge.scratchforge.base.common.item.*;
+import org.golde.forge.scratchforge.base.common.world.*;
+import org.golde.forge.scratchforge.base.helpers.*;
+
+import net.minecraft.block.*;
+import net.minecraft.block.material.*;
+import net.minecraft.client.*;
+import net.minecraft.client.audio.*;
+import net.minecraft.client.entity.*;
+import net.minecraft.client.gui.*;
+import net.minecraft.client.gui.achievement.*;
+import net.minecraft.client.gui.inventory.*;
+import net.minecraft.client.model.*;
+import net.minecraft.client.multiplayer.*;
+import net.minecraft.client.particle.*;
+import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.culling.*;
+import net.minecraft.client.renderer.entity.*;
+import net.minecraft.client.renderer.texture.*;
+import net.minecraft.client.renderer.tileentity.*;
+import net.minecraft.client.settings.*;
+import net.minecraft.command.*;
+import net.minecraft.crash.*;
+import net.minecraft.creativetab.*;
+import net.minecraft.dispenser.*;
+import net.minecraft.enchantment.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.ai.*;
+import net.minecraft.entity.boss.*;
+import net.minecraft.entity.effect.*;
+import net.minecraft.entity.item.*;
+import net.minecraft.entity.monster.*;
+import net.minecraft.entity.passive.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.entity.projectile.*;
+import net.minecraft.init.*;
+import net.minecraft.inventory.*;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.*;
+import net.minecraft.nbt.*;
+import net.minecraft.network.*;
+import net.minecraft.network.rcon.*;
+import net.minecraft.pathfinding.*;
+import net.minecraft.potion.*;
+import net.minecraft.profiler.*;
+import net.minecraft.server.*;
+import net.minecraft.server.dedicated.*;
+import net.minecraft.server.gui.*;
+import net.minecraft.server.integrated.*;
+import net.minecraft.server.management.*;
+import net.minecraft.src.*;
+import net.minecraft.stats.*;
+import net.minecraft.tileentity.*;
+import net.minecraft.util.*;
+import net.minecraft.village.*;
+import net.minecraft.world.*;
+import net.minecraft.world.biome.*;
+import net.minecraft.world.chunk.*;
+import net.minecraft.world.chunk.storage.*;
+import net.minecraft.world.demo.*;
+import net.minecraft.world.gen.*;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.layer.*;
+import net.minecraft.world.gen.structure.*;
+import net.minecraft.world.storage.*;
+
+import net.minecraftforge.classloading.*;
+import net.minecraftforge.client.*;
+import net.minecraftforge.client.event.*;
+import net.minecraftforge.client.event.sound.*;
+import net.minecraftforge.common.*;
+import net.minecraftforge.event.*;
+import net.minecraftforge.event.entity.*;
+import net.minecraftforge.event.entity.item.*;
+import net.minecraftforge.event.entity.living.*;
+import net.minecraftforge.event.entity.minecart.*;
+import net.minecraftforge.event.entity.player.*;
+import net.minecraftforge.event.terraingen.*;
+import net.minecraftforge.event.world.*;
+import net.minecraftforge.oredict.*;
+import net.minecraftforge.transformers.*;
+
+import cpw.mods.fml.common.*;
+import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.eventhandler.*;
+import cpw.mods.fml.common.gameevent.*;
+import cpw.mods.fml.common.gameevent.TickEvent.*;
+import cpw.mods.fml.common.gameevent.InputEvent.*;
+import cpw.mods.fml.common.gameevent.PlayerEvent.*;
+import cpw.mods.fml.common.Mod.*;
+import cpw.mods.fml.common.registry.*;
+import cpw.mods.fml.common.network.*;
+import cpw.mods.fml.relauncher.*;
+
+import org.apache.logging.log4j.*;
+
+import java.text.*;
+import java.util.*;
+import java.lang.*;
+
+import io.netty.buffer.*;
+import io.netty.channel.*;
+
+public class CommonProxy {
+
+	public static Scheduler scheduler = new Scheduler();
+
+	/* Block Variables */
+
+
+	/* BlockFlower Variables */
+
+
+	/* BlockPlant Variables */
+
+
+	/* Item Variables */
+	public static SpawnEgg spawnEgg;
+
+
+	/* Entity Variables */
+	/*Variables - Entity*/
+
+	public void preInit(FMLPreInitializationEvent event){
+		/* Block Constructor Calls */
+
+
+		/* BlockFlower Constructor Calls */
+
+
+		/* BlockPlant Constructor Calls */
+
+
+		/* Item Constructor Calls */
+		spawnEgg = new SpawnEgg(ForgeMod.BLOCK_ID, ForgeMod.CREATIVE_TAB);
+
+
+		/* Entity Constructor Calls */
+		createEntity(Mcentity_Test.class, Mcentity_Test.RAW_NAME, Mcentity_Test.NAME, Mcentity_Test.EGG_P, Mcentity_Test.EGG_S); //Biped
+
+	}
+
+	public void init(FMLInitializationEvent event){
+		MinecraftForge.EVENT_BUS.register(this);
+		MinecraftForge.TERRAIN_GEN_BUS.register(this);
+		FMLCommonHandler.instance().bus().register(this);
+
+		/* Recipe Registry */
+
+	}
+
+	public void serverLoad(FMLServerStartingEvent event) {
+		/* Command Registry */
+
+	}
+
+	@SubscribeEvent
+	public void onServerTick(TickEvent.ServerTickEvent event) {
+		scheduler.update();
+	}
+
+	public void generateSurface(World world, Random random, int chunkX, int chunkZ) {
+		for (int i = 0; i < 20; i++) {
+			int x = chunkX + random.nextInt(16) + 8;
+			int y = random.nextInt(128);
+			int z = chunkZ + random.nextInt(16) + 8;
+
+			/*Overworld world generation for flowers*/
+
+
+			/*Overworld world generation for plants*/
+
+
+		}
+	}
+
+	//TODO: Implement nether plant generation?
+	public void generateNether(World world, Random random, int chunkX, int chunkZ) {
+		for (int i = 0; i < 20; i++) {
+			int x = chunkX + random.nextInt(16) + 8;
+			int y = random.nextInt(128);
+			int z = chunkZ + random.nextInt(16) + 8;
+
+			/*Nether generation for flowers*/
+			/*WorldGen - Nether - Flowers*/
+
+			/*Nether generation for plants*/
+			/*WorldGen - Nether - Plant*/
+
+		}
+	}
+
+	public void createEntity(Class entityClass, String rawEntityName, String entityName, int solidColor, int spotColor) {
+		int id = EntityRegistry.findGlobalUniqueEntityId();
+		EntityRegistry.registerGlobalEntityID(entityClass, rawEntityName, id);
+		EntityRegistry.registerModEntity(entityClass, rawEntityName, id, ForgeMod.INSTANCE, 64, 1, true);
+		if(solidColor != -1 && spotColor != -1) {
+			createEgg(id, solidColor, spotColor);
+		}
+		//TODO: Add language
+	}
+
+	private void createEgg(int id, int solidColor, int spotColor) {
+		spawnEgg.entityEggs.put(Integer.valueOf(id), new EntityList.EntityEggInfo(id, solidColor, spotColor));
+	}
+
+
+	/*type:global*/
+
+
+
+
+
+	/*type:entity*/
+	/*model:Biped*/
+	public static class Mcentity_Test extends EntityCreature {
+		public static final String RAW_NAME = "Test";
+		public static final String NAME = "Test";
+		public static final boolean SPAWN_NATURALLY = false;
+		public static final int EGG_P = 0x66ffff;
+		public static final int EGG_S = 0x33ff33;
+
+		public Mcentity_Test(World world){
+			super(world);
+			this.tasks.addTask(0, new EntityAIWander(this, 0.6f));
+		}
+
+		@Override
+		protected boolean isAIEnabled() {
+			return this.tasks.taskEntries.size() > 0;
+		}
+
+		//Attack entity event
+		@Override
+		public boolean attackEntityFrom(DamageSource source, float damage) {
+
+			final VariableHolder variableHolder = new VariableHolder();
+			variableHolder.entity = this;
+			
+			if(source.isFireDamage()) {
+				//Fire damage
+			}
+			else if(source.isExplosion()) {
+				//explosion event
+			}
+			else if(source.isMagicDamage()) {
+				//Potion damage
+			}
+			else if(source.isProjectile()) {
+				//Arrows
+			}
+			else {
+				Entity damager = source.getEntity();
+				if(damager != null) {
+
+					if(damager instanceof EntityPlayer) {
+						//Player damaged
+					}
+					else {
+						//Entity did damage (wolf)
+					}
+
+
+				}
+				else {
+					//Fall damage or other
+				}
+			}
+
+
+			return super.attackEntityFrom(source, damage);
+		}
+
+		@Override
+		public void onDeath(DamageSource source) {
+
+			final VariableHolder variableHolder = new VariableHolder();
+			variableHolder.entity = this;
+
+			super.onDeath(source);
+		}
+
+
+	}
+
+
+}
